bee39bd2bbdddc7769ff436556903ea4eec5cfd3
nlnpare <nalineeeiam@gmail.com>
2022-12-14 13:25:54 +0700

    pictuce

---
+++ Solo Project/tiled/OutdoorsTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="OutdoorsTileset" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <image source="../../../Downloads/16x16 Outdoors Tileset/16x16 Outdoors Tileset/OutdoorsTileset.png" width="96" height="144"/>
+</tileset>
---
+++ Solo Project/tiled/demonstration.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="demonstration" tilewidth="32" tileheight="32" tilecount="1332" columns="36">
+ <image source="../../../Downloads/EPIC RPG World Pack - [FREE Demo]Ancient Ruins/EPIC RPG World Pack - [FREE Demo]Ancient Ruins - Copia/Tilesets/demonstration.png" width="1154" height="1210"/>
+</tileset>
